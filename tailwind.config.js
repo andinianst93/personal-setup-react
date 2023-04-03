b56c31d4fc1e7c0fb0ac49f5399c273078440bd9
andinianst93 <andinianst93@gmail.com>
2023-04-03 07:28:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
+    screens: {
+      sm: '480px',
+
+      md: '768px',
+
+      lg: '976px',
+
+      xl: '1440px',
+
+      xxl: '1800px',
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
